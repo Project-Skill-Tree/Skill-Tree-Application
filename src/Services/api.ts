9205d6f5b94ef8f71938cb49521d4a47143ed61b
Swatarianess <swatarianess@gmail.com>
2022-07-17 19:20:36 +0200

    Added all the files retrieved after installing the boilerplate 😊

---
+++ src/Services/api.ts
@@ -0,0 +1,28 @@
+import { Config } from '@/Config'
+import {
+  BaseQueryFn,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query/react'
+
+const baseQuery = fetchBaseQuery({ baseUrl: Config.API_URL })
+
+const baseQueryWithInterceptor: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions)
+  if (result.error && result.error.status === 401) {
+    // TODO Do something when there is an error in querying
+    console.error(result.error)
+  }
+  return result
+}
+
+export const api = createApi({
+  baseQuery: baseQueryWithInterceptor,
+  endpoints: () => ({}),
+})
---
+++ src/Theme/themes/default_dark/Images.ts
@@ -0,0 +1,11 @@
+/**
+ * Images should be stored in the `App/Images` directory and referenced using variables defined here.
+ */
+
+export default function () {
+  return {
+    logo: {
+      uri: 'https://thecodingmachine.github.io/react-native-project_skill_tree_application/img/TOM-small.png',
+    },
+  }
+}
